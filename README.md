3da03af50a7aa484c3bd7b0d41d14f47e552c5fa
Felix Stenbäck <felix.stenback@zenseact.com>
2023-11-03 16:07:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## How fast does the harmonic series converge based on float types
+
+Software inspired by A blog post by  Nick Higham
+
+See, https://nhigham.com/2018/12/03/half-precision-arithmetic-fp16-versus-bfloat16/
+
+## run
+```
+g++ main.cpp -Ihalf.hpp -Ibfloat16.h
+./a.out
+```
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstdint>
+
+#include "half.hpp"
+#include "bfloat16.h"
+
+int main(int argc, char** argv)
+{
+    long long steps{1};
+    float current = 0.f;
+    float previous = 0.f;
+    while (true)
+    {
+        current += static_cast<float>(1.0f) / steps;
+        steps++;
+        if (current == previous)
+        {
+            break;
+        }
+        previous = current;
+    }
+    std::cout << "float steps: " << steps << std::endl;
+
+    steps = 1;
+    half_float::half half_current{0.0};
+    half_float::half half_previous{0.0};
+    while (true)
+    {
+        half_current += half_float::half{1.0} / steps;
+        steps++;
+        if (half_current == half_previous)
+        {
+            break;
+        }
+        half_previous = half_current;
+    }
+    std::cout << "half_float steps: " << steps << std::endl;
+
+    steps = 1;
+    biovault::bfloat16_t bf_current{0.0};
+    biovault::bfloat16_t bf_previous{0.0};
+    while (true)
+    {
+        bf_current += biovault::bfloat16_t{1.0} / steps;
+        steps++;
+        if (bf_current == bf_previous)
+        {
+            break;
+        }
+        bf_previous = bf_current;
+    }
+    std::cout << "bf_float steps: " << steps << std::endl;
+    return 0;
+}
